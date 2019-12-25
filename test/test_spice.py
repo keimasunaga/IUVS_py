a098f36454682ec3cc168a604a8a94568032ca04
Kei Masunaga <keimasunaga@gmail.com>
2019-12-25 13:09:50 -0700

    testing iuvs and spice plots

---
+++ test/test_spice.py
@@ -0,0 +1,71 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import spiceypy as spice
+
+# print out the toolkit version
+print(spice.tkvrsn('TOOLKIT'))
+
+spice.furnsh('/Users/masunaga/work/spice_temp/CassMetaK.txt')
+
+step = 4000
+
+# we are going to get positions between these two dates
+utc = ['Jun 20, 2004', 'Dec 1, 2005']
+
+# get et values one and two, we could vectorize str2et
+etOne = spice.str2et(utc[0])
+etTwo = spice.str2et(utc[1])
+print('Et One: {}, ET Two: {}'.format(etOne, etTwo))
+
+# get times
+times = [x*(etTwo - etOne)/step + etOne for x in range(step)]
+
+# check first few times
+print(times[0:3])
+
+# run spkpos as a vectorized function
+positions, lightTimes = spice.spkpos('Cassini', times, 'J2000', 'NONE',
+                                     'SATURN BARYCENTER')
+
+# positions in a 3xN vector or XYZ positions
+print('Positions: ')
+print(positions[0])
+
+# light times is a N vector of time
+print('Light times: ')
+print(lightTimes[0])
+
+# clean up the kernels
+spice.kclear()
+
+# plot
+positions = np.asarray(positions).T # positions is a list, make it an ndarray for easier indexing
+fig = plt.figure(figsize=(9, 9))
+ax  = fig.add_subplot(111, projection='3d')
+ax.plot(positions[0], positions[1], positions[2])
+plt.title('SpiceyPy Cassini Position Example from Jun 20, 2004 to Dec 1, 2005')
+plt.show()
+"""
+plotly.offline.init_notebook_mode()
+threeDPlot = go.Scatter3d(x=positions[:, 0],  # X coordinates
+                          y=positions[:, 1],  # Y coordinates
+                          z=positions[:, 2],  # Z coordinates
+                          name='Cassini',
+                          mode='lines',
+                          line=dict(width=3))
+
+barycenter = go.Scatter3d(x=[0],
+                          y=[0],
+                          z=[0],
+                          name='bc',
+                          mode='marker',
+                          marker=dict(size=10, color='orange'))
+
+data = [threeDPlot, barycenter]
+
+layout = go.Layout(title="SpiceyPy Cassini Position Example")
+
+fig = dict(data=data, layout=layout)
+py.iplot(fig)
+"""
