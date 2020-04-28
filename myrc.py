a60ecbcff8494ba86ecb46ba2559dce1c9574da0
Kei Masunaga <keimasunaga@gmail.com>
2020-04-28 01:29:59 -0600

    fixed minor bug

--- myrc.py
+++ myrc.py
@@ -2,6 +2,6 @@
 
 class IuvRc:
     def __init__(self):
-        dataloc = '/Volumes/Gravity/work/data/maven_iuvs/'
-        saveloc = '/Users/masunaga/work/save_data/iuvs/'
-        spiceloc = '/Volumes/Gravity/work/data/maven_iuvs/spice/'
+        self.dataloc = '/Volumes/Gravity/work/data/maven_iuvs/'
+        self.saveloc = '/Users/masunaga/work/save_data/iuvs/'
+        self.spiceloc = '/Volumes/Gravity/work/data/maven_iuvs/spice/'
