39ce4f8430689cb9c110df645b082381a9787429
Kei Masunaga <keimasunaga@gmail.com>
2020-04-28 01:08:06 -0600

    made an rc tool

---
+++ myrc.py
@@ -0,0 +1,7 @@
+
+
+class IuvRc:
+    def __init__(self):
+        dataloc = '/Volumes/Gravity/work/data/maven_iuvs/'
+        saveloc = '/Users/masunaga/work/save_data/iuvs/'
+        spiceloc = '/Volumes/Gravity/work/data/maven_iuvs/spice/'
