6b708d3c78106f3a49558e8b1cc9804eb1eeefb6
Kei Masunaga <keimasunaga@gmail.com>
2020-04-29 15:45:16 -0600

    minor fix: fix a working directory path
    Path to the working directory is fixed due to reorganizing the python directory tree

--- iuvinit.py
+++ iuvinit.py
@@ -4,7 +4,7 @@ This file has to be imported first when you use IUVS lib.
 
 ## Add some directories to Pythonpath
 import sys
-workpath = '/Users/masunaga/work/python_git/py_iuvs/'
+workpath = '/Users/masunaga/work/python_git/maven/iuvs/'
 dirs = ['test', 'script']
 [sys.path.append(workpath + idir) for idir in dirs]
 
