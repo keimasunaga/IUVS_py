68cf6ecd42955c624040d6b97e9f8afd9f9bf570
Kei Masunaga <keimasunaga@gmail.com>
2020-04-30 00:33:24 -0600

    Fix the saveloc path
    Set a new savepath as product in the project directory

--- myrc.py
+++ myrc.py
@@ -3,5 +3,5 @@
 class IuvRc:
     def __init__(self):
         self.dataloc = '/Volumes/Gravity/work/data/maven_iuvs/'
-        self.saveloc = '/Users/masunaga/work/save_data/iuvs/'
+        self.saveloc = '/Users/masunaga/work/python_git/maven/iuvs/product/'
         self.spiceloc = '/Volumes/Gravity/work/data/maven_iuvs/spice/'
