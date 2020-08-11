056418c4b337bb95180f7dd8db4b29c535e529c1
Kei Masunaga <keimasunaga@gmail.com>
2020-08-11 14:56:07 +0900

    Add mcp_volt and dayside functions

--- iuvtools/info.py
+++ iuvtools/info.py
@@ -14,3 +14,14 @@ def get_channel(hdul):
 def get_solar_lon(hdul):
     Ls = hdul['Observation'].data['Solar_Longitude']
     return Ls[0]
+
+def get_mcp_volt(hdul):
+    mcp_volt = hdul['Observation'].data['MCP_Volt']
+    return mcp_volt[0]
+
+def dayside(hdul): ## Check if the voltage value is correct later
+    mcp_volt = get_mcp_volt(hdul)
+    if mcp_volt < 700:
+        return True
+    else:
+        return False
