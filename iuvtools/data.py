22e0aedf44eefd122acc86034e12fc0bc0bcd39d
Kei Masunaga <keimasunaga@gmail.com>
2020-09-27 18:54:15 -0600

    Add detector img tools
    Retrive detector image (n_spec x n_space) from primary/detector_dark_subtract

--- iuvtools/data.py
+++ iuvtools/data.py
@@ -1,3 +1,4 @@
+import numpy as np
 # data
 def get_primary(hdul):
     primary = hdul['primary'].data
@@ -11,3 +12,18 @@ def get_wv(hdul):
     'return wavelength in nm, dim = (n_space, n_spectral)'
     wv = hdul['Observation'].data['Wavelength'][0]
     return wv
+
+
+def get_primary_detector_img(hdul, mean=True, i_integration=None):
+    if mean:
+        img = np.nanmean(hdul['primary'].data, axis=0)
+    else:
+        img = hdul['primary'].data[i_integration]
+    return img
+
+def get_counts_detector_img(hdul, mean=True, i_integration=None):
+    if mean:
+        img = np.nanmean(hdul['detector_dark_subtracted'].data, axis=0)
+    else:
+        img = hdul['detector_dark_subtracted'].data[i_integration]
+    return img
