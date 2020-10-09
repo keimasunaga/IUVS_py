4d050dd6eefb3703ac4acde9a67cb34f047bfc9c
Kei Masunaga <keimasunaga@gmail.com>
2020-10-09 15:08:22 -0600

    Add a class OrbitInfo
    This is a copy of other functions in the same module

--- orbit_info.py
+++ orbit_info.py
@@ -75,3 +75,61 @@ def test():
     print(sDt, eDt)
     print(Dt_peri, Dt_apo)
     print(orb)
+
+
+class OrbitInfo:
+    def __init__(self):
+        datapath = '/Volumes/Fenix/work/data/misc/spice/naif/MAVEN/kernels/spk/'
+        f = glob(datapath+'maven_orb_rec_*.orb')
+
+        keys = ['orbit_number', 'utc_peri', 'Event_SCLK_PERI',  'utc_apo', 'SolLon',  'SolLat', 'SCLon', 'SCLat',  'Alt', 'SolDist']
+        farr = np.array([])
+        for i, ifile in enumerate(f):
+            if i==0:
+                df = pd.read_fwf(ifile)
+                df = df.drop(df.index[0])
+            else:
+                df2 = pd.read_fwf(ifile)
+                df2 = df2.drop(df2.index[0])
+                df = df.append(df2)
+
+        df.columns = keys
+        df = df.reset_index(drop=True)
+        convert_dict = {'orbit_number': int, 'SolLon': float, 'SolLat':float, 'SCLon':float, 'SCLat':float,  'Alt':float, 'SolDist':float}
+        self.df = df.astype(convert_dict)
+
+    def get_dic_orbfiles(self):
+        dic = self.df.to_dict(orient='list')
+        return dic
+
+    def get_Dt_peri(self, orbit_number):
+        df_selec = self.df[self.df['orbit_number'] == orbit_number]
+        Dt_peri = pd.to_datetime(df_selec['utc_peri']).tolist()[0].to_pydatetime().replace(tzinfo=timezone.utc)
+        return Dt_peri
+
+    def get_Dt_apo(self, orbit_number):
+        df_selec = self.df[self.df['orbit_number'] == orbit_number]
+        Dt_apo = pd.to_datetime(df_selec['utc_apo']).tolist()[0].to_pydatetime().replace(tzinfo=timezone.utc)
+        return Dt_apo
+
+    def get_Dtlim(self, orbit_number, center='periapse'):
+        if center == 'periapse':
+            df_selec = self.df[self.df['orbit_number'] == orbit_number]
+            df_selec_pre = self.df[self.df['orbit_number'] == orbit_number - 1]
+            eDt = pd.to_datetime(df_selec['utc_apo']).tolist()[0].to_pydatetime().replace(tzinfo=timezone.utc)
+            sDt = pd.to_datetime(df_selec_pre['utc_apo']).tolist()[0].to_pydatetime().replace(tzinfo=timezone.utc)
+        elif center == 'apoapse':
+            df_selec = self.df[self.df['orbit_number'] == orbit_number]
+            df_selec_lat = self.df[self.df['orbit_number'] == orbit_number + 1]
+            sDt = pd.to_datetime(df_selec['utc_peri']).tolist()[0].to_pydatetime().replace(tzinfo=timezone.utc)
+            eDt = pd.to_datetime(df_selec_lat['utc_peri']).tolist()[0].to_pydatetime().replace(tzinfo=timezone.utc)
+        return [sDt, eDt]
+
+    def get_orbit_number(self, Dt):
+        dic = self.get_dic_orbfiles()
+        orbit_number_all = np.array(dic['orbit_number'])
+        Dt_peri = np.array([datetime.strptime(istr, '%Y %b %d %H:%M:%S').replace(tzinfo=timezone.utc) for istr in dic['utc_peri']]) #add tzinfo here too?
+        Dt_apo = np.array([datetime.strptime(istr, '%Y %b %d %H:%M:%S').replace(tzinfo=timezone.utc) for istr in dic['utc_apo']]) #add tzinfo here too?
+        idx_orb = np.where((Dt >= Dt_peri[0:-1]) & (Dt < Dt_peri[1:]))[0][0]
+        orbit_number = orbit_number_all[idx_orb]
+        return orbit_number
