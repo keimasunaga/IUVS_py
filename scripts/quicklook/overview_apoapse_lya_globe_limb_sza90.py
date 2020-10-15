4232a48c7c7fb241b54f4680a207ff85e1d9cabe
Kei Masunaga <keimasunaga@gmail.com>
2020-10-15 15:05:34 -0600

    Change to Mike's colorbar

--- scripts/quicklook/overview_apoapse_lya_globe_limb_sza90.py
+++ scripts/quicklook/overview_apoapse_lya_globe_limb_sza90.py
@@ -58,7 +58,7 @@ def get_iuv_sw_euv_data(orbit_number):
     return dic_iuvs, dic_sw, dic_euv
 
 
-def plot_overvew(alt_default=True, selec_region=[0,1,2,3,4,5]):
+def plot_overview(alt_default=True, selec_region=[0,1,2,3,4,5]):
     args = [i for i in sys.argv]
     if len(args)>1:
         alt_default = args[1]
@@ -67,7 +67,12 @@ def plot_overvew(alt_default=True, selec_region=[0,1,2,3,4,5]):
         else:
             alt_default = True
 
-    color = ['C0', 'C1', 'C2', 'C3', 'C4', 'C5']
+    color = [[0.22719513, 0.24914381, 0.40675258, 1. ],
+             [0.53262074, 0.35198344, 0.50904462, 1. ],
+             [0.90686879, 0.62111989, 0.57090984, 1. ],
+             [0.91383359, 0.82674759, 0.55570039, 1. ],
+             [0.55800763, 0.76309657, 0.59473906, 1. ],
+             [0.23115755, 0.46685701, 0.50050539, 1. ]]
     orbit_arr = np.arange(700, 5500)
     for ith, iregion in enumerate(selec_region):
         data = []
