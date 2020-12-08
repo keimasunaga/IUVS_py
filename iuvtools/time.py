61bca487060c84e7346af266f042385fd108b39f
Kei Masunaga <keimasunaga@gmail.com>
2020-12-07 19:41:31 -0700

    Add Dt2str
    Convert datetime to string format

--- iuvtools/time.py
+++ iuvtools/time.py
@@ -20,3 +20,19 @@ def get_timeDt(hdul):
     utc = hdul['Integration'].data['utc']
     timeDt = np.array([utc2Dt(iutc) for iutc in utc])
     return timeDt
+
+def Dt2str(timeDt):
+    '''
+    Converts datetime to string format
+    argment:
+        timeDt: a datetime or a list of those
+    returns:
+        str_time: a string time in string format or list or array of them
+    '''
+    if np.size(timeDt) == 1:
+        if type(timeDt) is datetime:
+            return timeDt.strftime('%Y-%m-%dT%H:%M:%S')
+        else:
+            return timeDt[0].strftime('%Y-%m-%dT%H:%M:%S')
+    else:
+        return [iDt.strftime('%Y-%m-%dT%H:%M:%S') for iDt in timeDt]
